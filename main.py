f326ec3b2834244598af83e12214971c9bf482f9
Svetla Alexandrova <121426643+Svetla-Alexandrova@users.noreply.github.com>
2022-12-29 13:00:20 +0000

    Manipulate filenames - changing with new name

--- main.py
+++ main.py
@@ -1,16 +1,28 @@
 from pathlib import Path
-p1 = Path('files/abc.txt')
-print(p1)
 
-if p1.exists():
-  with open(p1, 'r') as file:
-    print(file.read())
+# Using pathlib to find, create files
+# p1 = Path('files/abc.txt')
+# print(p1)
 
-print(p1.name)
-print(p1.stem)
-print(p1.suffix)
+# if p1.exists():
+#   with open(p1, 'r') as file:
+#     print(file.read())
 
-p2 = Path('files')
-for item in p2.iterdir():
-  print(item, type(item))
+# print(p1.name)
+# print(p1.stem)
+# print(p1.suffix)
+
+# p2 = Path('files')
+# for item in p2.iterdir():
+#   print(item, type(item))
+  
+# Using pathlib to manipulate files - adding prefix to all filenames in folder
+root_dir = Path('files')
+file_paths = root_dir.iterdir()
+# print(list(file_paths))
+
+for path in file_paths:
+  new_filename = "new-" + path.stem + path.suffix
+  new_filepath = path.with_name(new_filename)
+  path.rename(new_filepath)
   
