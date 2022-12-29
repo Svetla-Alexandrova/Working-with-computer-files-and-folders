c430c893173b665da536454e58b88455cbea02e0
Svetla Alexandrova <121426643+Svetla-Alexandrova@users.noreply.github.com>
2022-12-29 13:21:37 +0000

    Rename files in sub-folders

--- main.py
+++ main.py
@@ -17,12 +17,27 @@ from pathlib import Path
 #   print(item, type(item))
   
 # Using pathlib to manipulate files - adding prefix to all filenames in folder
+# root_dir = Path('files')
+# file_paths = root_dir.iterdir()
+# # print(list(file_paths))
+
+# for path in file_paths:
+#   new_filename = "new-" + path.stem + path.suffix
+#   new_filepath = path.with_name(new_filename)
+#   path.rename(new_filepath)
+
+# Rename all files based on folder
+
 root_dir = Path('files')
-file_paths = root_dir.iterdir()
-# print(list(file_paths))
+# file_paths = root_dir.iterdir()
+# Elegant way to go in file folders and thus escaping the nested "for-loop" below
+file_paths = root_dir.glob("**/*")
 
 for path in file_paths:
-  new_filename = "new-" + path.stem + path.suffix
-  new_filepath = path.with_name(new_filename)
-  path.rename(new_filepath)
+  if path.is_file():
+    parent_folder = path.parts[-2]
+    new_filename = parent_folder + "-" + path.name
+    print(new_filename)
+    new_filepath = path.with_name(new_filename)
+    path.rename(new_filepath)
   
